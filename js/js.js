c6a17b257f04f1a2109dc8fdfe3a8e7c772224bd
diana chuctaya pacheco <leydiana.ch2015@gmail.com>
2017-07-21 17:21:43 -0500

    mejorando

--- js/js.js
+++ js/js.js
@@ -2,28 +2,60 @@ var celdas = document.getElementsByTagName('td');
 for (var i = 0; i < celdas.length; i++) {
     celdas[i].addEventListener('click',redirect,false);
 }
+var numAsientos;
+
 function redirect(event){
-    document.getElementById("mostrar").innerHTML=(event.target.textContent);
+    numAsientos = event.target.textContent ;
+    var asiento=(event.target.textContent);
+    var mostrar=document.getElementById("mostrar");
+    mostrar.innerHTML=asiento;
+    (event.target).style.backgroundColor = ((event.target).style.backgroundColor=='rgb(248, 237, 80)') ? 'transparent' : '#6973AD';
 }
+   var N = 32; // Número de asientos
+   var asientos = [];
+   for (var i = 0; i < N; i++) {
+      asientos[i] = ' ';
+   }
 
-//+++++++++++++++++++++++++++++++++++
-
-var asiento = document.getElementById('mostrar')
-var name = document.getElementById("nombre").value;
-var resultado = document.getElementById('mostrar');
-resultado.innerHTML=name;
-
-var apell= document.getElementById("apellido").value;
-var identidad = document.getElementById('dni').value;
+   
+    var buscaDni = document.getElementById('ingresadni').value;
+    var name = document.getElementById("nombre").value;
+    
+    var apell= document.getElementById("apellido").value;
+    var identidad = document.getElementById('dni').value;
     
 function buscar() {
+    // var asiento =celdas;
+    document.getElementById("nombre").innerHTML=name;
+    document.getElementById('apellido').innerHTML=apell;
+    document.getElementById('dni').innerHTML=identidad;
+}
+function reservar() {
+    var asiento =parseInt(numAsientos);
     var name = document.getElementById("nombre").value;
     var apell= document.getElementById("apellido").value;
     var identidad = document.getElementById('dni').value;
-    document.getElementById("nombre").innerHTML=name;    
+     asientos[asiento] = {
+               nombre : name,
+               apellido :apell,
+               asientoOcupado :asiento,
+               dni: identidad
+    }
+    document.getElementById('usuario').innerHTML= asiento+'<br>'+name +'<br>'+ apell +'<br>'+identidad;
+
+ 
 }
-function reservar() {
+function cancelar() {
+    if (buscaDni =asientos.dni)
+     asientos[asiento] = {
+               nombre : name,
+               apellido :apell,
+               asientoOcupado :asiento,
+               dni: identidad
+    }
+
     
-
-
+}
+function limpiar(){
+document.getElementById('usuario').innerHTML=' ';
 }
